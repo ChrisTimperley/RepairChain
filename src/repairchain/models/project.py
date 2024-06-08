e29a71be6d00c29ae74063acf612e6bddeec4c03
ChrisTimperley <christimperley@googlemail.com>
2024-06-07 23:17:13 -0400

    added ProjectKind enum to distinguish between C, Kernel, Java projects

--- src/repairchain/models/project.py
+++ src/repairchain/models/project.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import enum
 import typing as t
 from dataclasses import dataclass
 
@@ -9,20 +10,30 @@ if t.TYPE_CHECKING:
     from pathlib import Path
 
 
+class ProjectKind(str, enum.Enum):
+    C = "c"
+    KERNEL = "kernel"
+    JAVA = "java"
+
+
 @dataclass
 class Project:
+    kind: ProjectKind
     repository: git.Repo
     triggering_commit: git.Commit
 
     @classmethod
     def build(
         cls,
+        kind: str,
         repository_path: Path,
         triggering_commit_sha: str,
     ) -> t.Self:
+        project_kind = ProjectKind(kind)
         repository = git.Repo(repository_path)
         commit = repository.commit(triggering_commit_sha)
         return cls(
+            kind=project_kind,
             repository=repository,
             triggering_commit=commit,
         )
