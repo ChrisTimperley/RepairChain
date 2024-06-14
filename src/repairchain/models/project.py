68816518f34e4c6d34f891678ee4edea77ff427e
ChrisTimperley <christimperley@googlemail.com>
2024-06-14 11:08:01 -0400

    added head field to Project

--- src/repairchain/models/project.py
+++ src/repairchain/models/project.py
@@ -23,6 +23,7 @@ class Project:
     kind: ProjectKind
     image: str
     repository: git.Repo
+    head: git.Commit
     triggering_commit: git.Commit
     regression_test_command: str
     crash_command: str
@@ -40,12 +41,14 @@ class Project:
     ) -> t.Self:
         project_kind = ProjectKind(kind)
         repository = git.Repo(repository_path)
+        head = repository.head.commit
         commit = repository.commit(triggering_commit_sha)
         sanitizer_report = SanitizerReport(sanitizer="ASAN")  # FIXME: placeholder
         return cls(
             kind=project_kind,
             image=image,
             repository=repository,
+            head=head,
             triggering_commit=commit,
             regression_test_command=regression_test_command,
             crash_command=crash_command,
