a3de37452fba1bc2cfe6b5ccc6a1c317c48fb060
ChrisTimperley <christimperley@googlemail.com>
2024-06-14 12:23:12 -0400

    added index_statements action

---
+++ src/repairchain/actions/index_statements.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+__all__ = ("index_statements",)
+
+import typing as t
+
+if t.TYPE_CHECKING:
+    import git
+    import kaskara.functions
+
+    from repairchain.models.project import Project
+
+
+def index_statements(
+    project: Project,
+    *,
+    version: git.Commit | None = None,
+    restrict_to_files: list[str] | None = None,
+    restrict_to_functions: list[kaskara.functions.Function] | None = None,
+) -> kaskara.functions.ProgramFunctions:
+    """Produces an index of the statements in a given project.
+
+    If `version` is provided, statements are indexed at that version,
+    otherwise, statements are indexed at the current version (i.e., HEAD).
+    """
+    raise NotImplementedError
