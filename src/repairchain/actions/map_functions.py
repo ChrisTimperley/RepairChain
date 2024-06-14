447a9ded72d9c8cc34e717253baa05af5e17958d
ChrisTimperley <christimperley@googlemail.com>
2024-06-14 11:07:22 -0400

    added API for map_functions action

---
+++ src/repairchain/actions/map_functions.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+import typing as t
+
+if t.TYPE_CHECKING:
+    import git
+    import kaskara.functions
+
+    from repairchain.models.project import Project
+
+
+def map_functions(
+    project: Project,
+    functions: list[kaskara.functions.Function],
+    from_version: git.Commit,
+    to_version: git.Commit,
+    old_function_index: kaskara.functions.ProgramFunctions,
+    new_function_index: kaskara.functions.ProgramFunctions,
+) -> list[kaskara.functions.Function]:
+    raise NotImplementedError
