53297a0d974a129f6a9daae2f338847069e71071
ChrisTimperley <christimperley@googlemail.com>
2024-06-27 09:02:07 -0400

    implemented writing of patch files to disk

--- src/repairchain/repairchain.py
+++ src/repairchain/repairchain.py
@@ -4,6 +4,8 @@ __all__ = ("run",)
 
 import typing as t
 
+from loguru import logger
+
 from repairchain.actions.repair import repair
 
 if t.TYPE_CHECKING:
@@ -18,9 +20,18 @@ def run(
     save_patches_to_dir: Path,
     stop_early: bool = True,
 ) -> None:
+    save_patches_to_dir.mkdir(exist_ok=True, parents=True)
+
     patches = repair(project, stop_early=stop_early)
 
-    # TODO format the repairs and write them to a specified file (for VERSATIL)
-    print(f"saving patches to {save_patches_to_dir}...")
-    print(patches)
-    raise NotImplementedError
+    if not patches:
+        logger.info("no patches found")
+        return
+
+    logger.info(f"saving {len(patches)} patches to {save_patches_to_dir}...")
+    for patch_no, patch in enumerate(patches):
+        patch_filename = save_patches_to_dir / f"patch_{patch_no}.diff"
+        diff_content = str(patch)
+        with patch_filename.open("w") as file:
+            file.write(diff_content)
+    logger.info("saved patches")
