5c0418e96c3eaf45464d4034b202f97de5b15b0f
Claire Le Goues <clegoues@cs.cmu.edu>
2024-06-27 13:21:32 -0400

    initial dd implementation, test not hooked up

--- src/repairchain/actions/minimize_diff.py
+++ src/repairchain/actions/minimize_diff.py
@@ -10,6 +10,22 @@ if t.TYPE_CHECKING:
     from repairchain.models.diff import Diff
 
 
+def subset_from_bitvector(elements, bitvector):
+    subset = []
+    index = 0
+    while bitvector > 0:
+        if bitvector & 1:
+            subset.append(elements[index])
+        bitvector >>= 1
+        index += 1
+    return subset
+
+
+def test(elements, bitvector):
+    subset = subset_from_bitvector(elements, bitvector)
+    # make some dummy test here so I can check that it's working
+    raise NotImplemented
+    
 def minimize_diff(
     repo: git.Repo,
     triggering_commit: git.Commit,
@@ -23,5 +39,35 @@ def minimize_diff(
     # - transform bitvector into a Diff using Diff.from_file_hunks
     hunks = list(triggering_diff.file_hunks)
     _num_hunks = len(hunks)
+    hunks_bitvec = (1 << _num_hunks) - 1
 
-    raise NotImplementedError
+    n = _num_hunks    
+    granularity = 1
+
+    while granularity < n:
+        progress = False
+        subset = 0
+
+        while subset < granularity:
+            next_try = 0
+
+            for i in range(n):
+                if (i // (n // granularity)) % 2 == subset:
+                    next_try |= (1 << i)
+
+            next_try &= hunks_bitvec
+
+            if next_try and not test(next_try):
+                hunks_bitvec = next_try
+                n = hunks_bitvec.bit_length()
+                granularity = max(granularity - 1, 1)
+                progress = True
+                break
+
+            subset += 1
+
+        if not progress:
+            granularity *= 2
+
+    return subset_from_bitvector(hunks,hunks_bitvec)
+
