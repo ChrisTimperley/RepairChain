71af43d0b989359a127de8bc75cd741e5b91cb78
ChrisTimperley <christimperley@googlemail.com>
2024-06-28 11:31:21 -0400

    implemented bulk of validate and validate_patch

--- src/repairchain/actions/validate.py
+++ src/repairchain/actions/validate.py
@@ -4,31 +4,59 @@ __all__ = ("validate", "validate_patch")
 
 import typing as t
 
+from repairchain.errors import BuildFailure
+from repairchain.models.patch_outcome import PatchOutcome
+
 if t.TYPE_CHECKING:
     import git
 
     from repairchain.models.diff import Diff
-    from repairchain.models.patch_outcome import PatchOutcome
     from repairchain.models.project import Project
 
 
 def validate_patch(
     project: Project,
-    commit: git.Commit,
     diff: Diff,
+    *,
+    commit: git.Commit | None = None,
 ) -> PatchOutcome:
     """Applies a given patch to a specific version of a project and returns the outcome."""
-    raise NotImplementedError
+    try:
+        with project.provision(version=commit, diff=diff) as container:
+            # TODO run PoV
+
+            if not container.run_regression_tests():
+                return PatchOutcome.FAILED
+
+            return PatchOutcome.PASSED
+
+    except BuildFailure:
+        return PatchOutcome.FAILED_TO_BUILD
 
 
 def validate(
     project: Project,
     candidates: list[Diff],
     *,
+    commit: git.Commit | None = None,
     stop_early: bool = False,
 ) -> list[Diff]:
     """Validates the generated patches and returns a list of valid patches.
 
     If `stop_early` is True, the validation process will stop as soon as a valid patch is found.
     """
-    raise NotImplementedError
+    # FIXME do this in parallel!
+    repairs: list[Diff] = []
+
+    for candidate in candidates:
+        outcome = validate_patch(
+            project=project,
+            diff=candidate,
+            commit=commit,
+        )
+        if outcome == PatchOutcome.PASSED:
+            repairs.append(candidate)
+            if stop_early:
+                break
+
+    return repairs
