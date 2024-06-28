460294730bd3b657717a4ab49ef364bc726575f9
Claire Le Goues <clegoues@cs.cmu.edu>
2024-06-28 12:51:49 -0400

    make a class for diff minimization

--- src/repairchain/actions/minimize_diff.py
+++ src/repairchain/actions/minimize_diff.py
@@ -2,67 +2,67 @@ from __future__ import annotations
 
 import typing as t
 
-from repairchain.actions.commit_to_diff import commit_to_diff
 from repairchain.models.patch_outcome import PatchOutcome
+from repairchain.models.project import Project
 
 if t.TYPE_CHECKING:
-    import git
 
     from repairchain.models.diff import Diff
 
 
-def split(c: frozenset[int], n: int) -> list[list[int]]:
-    subsets = []
-    start = 0
-    for i in range(n):
-        subset = c[start:start + (len(c) - start) // (n - i)]
-        if len(subset) > 0:
-            subsets.append(frozenset(subset))
-        start += len(subset)
-    return subsets
+class DiffMinimizer:
 
+    triggering_diff: Diff
 
-# this is just for testing; need to connect to actual validation.
-def test(patch: set) -> PatchOutcome:
-    if (3 in patch and 0 in patch):  # noqa: PLR2004
-        return PatchOutcome.FAILED
-    return PatchOutcome.PASSED
+    def __init__(self, triggering_diff: Diff) -> DiffMinimizer.t:
+        self.triggering_diff = triggering_diff
 
+    @staticmethod
+    def split(c: frozenset[int], n: int) -> list[list[int]]:
+        subsets = []
+        start = 0
+        for i in range(n):
+            subset = c[start:start + (len(c) - start) // (n - i)]
+            if len(subset) > 0:
+                subsets.append(frozenset(subset))
+            start += len(subset)
+        return subsets
 
-def test_with_cache(cache: dict[list[int], PatchOutcome], subset: list[int]) -> PatchOutcome:
-    if subset in cache:
-        return cache[subset]
-    outcome = test(subset)
-    cache[subset] = outcome
-    return outcome
+    # this is just for testing; need to connect to actual validation.
+    def _test(self, patch: set) -> PatchOutcome:
+        if (3 in patch and 0 in patch):  # noqa: PLR2004
+            return PatchOutcome.FAILED
+        return PatchOutcome.PASSED
 
+    def _test_with_cache(self, cache: dict[list[int], PatchOutcome], subset: list[int]) -> PatchOutcome:
+        if subset in cache:
+            return cache[subset]
+        outcome = self._test(subset)
+        cache[subset] = outcome
+        return outcome
 
-def minimize_diff(
-    repo: git.Repo,
-    triggering_commit: git.Commit,
-) -> Diff:
-    triggering_diff = commit_to_diff(triggering_commit)
+    def minimize_diff(self) -> Diff:
 
-    test_cache = {}
-    hunks = list(triggering_diff.file_hunks)
-    c_fail = frozenset(range(len(hunks)))
+        test_cache = {}
+        hunks = list(self.triggering_diff.file_hunks)
+        c_fail = frozenset(range(len(hunks)))
 
-    assert test_with_cache(test_cache, c_fail) == PatchOutcome.FAILED
+        assert self._test_with_cache(test_cache, c_fail) == PatchOutcome.FAILED
 
-    granularity = 2
+        granularity = 2
+
     # Main loop
-    while granularity >= 1:
-        subsets = split(list(c_fail), granularity)
-        reduced = False
-        for subset in subsets:
-            complement = c_fail - frozenset(subset)
+        while granularity >= 1:
+            subsets = DiffMinimizer.split(list(c_fail), granularity)
+            reduced = False
+            for subset in subsets:
+                complement = c_fail - frozenset(subset)
 
-            if test_with_cache(test_cache, complement) == PatchOutcome.FAILED:
-                c_fail = complement
-                granularity = max(granularity - 1, 2)
-                reduced = True
-        if not reduced:
-            if granularity >= len(c_fail):
+                if self._test_with_cache(test_cache, complement) == PatchOutcome.FAILED:
+                    c_fail = complement
+                    granularity = max(granularity - 1, 2)
+                    reduced = True
+            if not reduced and granularity >= len(c_fail):
                 return c_fail
             granularity = min(granularity * 2, len(c_fail))
-    return c_fail
+        return c_fail
--- test/test_minimize_diff.py
+++ test/test_minimize_diff.py
@@ -1,12 +1,14 @@
 import git  # noqa: INP001
 
-from repairchain.actions.minimize_diff import minimize_diff
+from repairchain.actions.commit_to_diff import commit_to_diff
+from repairchain.actions.minimize_diff import DiffMinimizer
 
 
 def test_minimize_diff_simple() -> None:
     repository = git.Repo("/home/clegoues/aixcc/challenge-001-exemplar/src")
     commit = repository.commit("426d4a428a9c6aa89f366d1867fae55b4ebd6b7f")
-    minimal = minimize_diff(repository,commit)
+    minimizer = DiffMinimizer(commit_to_diff(commit))
+    minimal = minimizer.minimize_diff()
     assert len(minimal) == 2  # noqa: PLR2004
     assert 0 in minimal
-    assert 3 in minimal
+    assert 3 in minimal  # noqa: PLR2004
