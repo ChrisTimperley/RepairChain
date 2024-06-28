442412383907e62fdb940972aa8cb5fc43525e62
Claire Le Goues <clegoues@cs.cmu.edu>
2024-06-28 13:10:51 -0400

    make dumb test still work

--- test/test_minimize_diff.py
+++ test/test_minimize_diff.py
@@ -8,6 +8,7 @@ def test_minimize_diff_simple() -> None:
     repository = git.Repo("/home/clegoues/aixcc/challenge-001-exemplar/src")
     commit = repository.commit("426d4a428a9c6aa89f366d1867fae55b4ebd6b7f")
     minimizer = DiffMinimizer(commit_to_diff(commit))
+    minimizer.do_dumb_test()
     minimal = minimizer.minimize_diff()
     assert len(minimal) == 2  # noqa: PLR2004
     assert 0 in minimal
