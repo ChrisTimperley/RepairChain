94f64368464e74fbfc21201084a3452bf95c51c8
Claire Le Goues <clegoues@cs.cmu.edu>
2024-06-28 13:15:34 -0400

    keep dumb test passing

--- test/test_minimize_diff.py
+++ test/test_minimize_diff.py
@@ -7,7 +7,7 @@ from repairchain.actions.minimize_diff import DiffMinimizer
 def test_minimize_diff_simple() -> None:
     repository = git.Repo("/home/clegoues/aixcc/challenge-001-exemplar/src")
     commit = repository.commit("426d4a428a9c6aa89f366d1867fae55b4ebd6b7f")
-    minimizer = DiffMinimizer(commit_to_diff(commit))
+    minimizer = DiffMinimizer(None, commit_to_diff(commit))
     minimizer.do_dumb_test()
     minimal = minimizer.minimize_diff()
     assert len(minimal) == 2  # noqa: PLR2004
