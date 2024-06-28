970b797520faf413c4a0da15e2d6cc0b4fad0687
Claire Le Goues <clegoues@cs.cmu.edu>
2024-06-28 12:31:40 -0400

    linter happy on test

--- test/test_minimize_diff.py
+++ test/test_minimize_diff.py
@@ -1,9 +1,12 @@
-from repairchain.actions.minimize_diff import minimize_diff
-import git
+import git  # noqa: INP001
 
-# 
-def test_minimize_diff_simple():
+from repairchain.actions.minimize_diff import minimize_diff
+
+
+def test_minimize_diff_simple() -> None:
     repository = git.Repo("/home/clegoues/aixcc/challenge-001-exemplar/src")
     commit = repository.commit("426d4a428a9c6aa89f366d1867fae55b4ebd6b7f")
     minimal = minimize_diff(repository,commit)
-    assert len(minimal) == 2 and 0 in minimal and 3 in minimal 
+    assert len(minimal) == 2
+    assert 0 in minimal
+    assert 3 in minimal
