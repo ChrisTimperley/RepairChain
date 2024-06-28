49cddb7d6552b58e8b7c175513c1115f612aa4b8
ChrisTimperley <christimperley@googlemail.com>
2024-06-28 19:40:04 -0400

    bug fix: __main__ was broken

--- src/repairchain/__main__.py
+++ src/repairchain/__main__.py
@@ -1,7 +1,4 @@
 import repairchain.cli
 
-commit = "8e2a8e613fe5b6f03cb8e0c27180a468671f03a8"
-repository = "git@github.com:VERSATIL-GrammaTech/challenge-004-nginx-source.git"
-
 if __name__ == "__main__":
-    repairchain.cli.cli(repository, commit)
+    repairchain.cli.cli()
