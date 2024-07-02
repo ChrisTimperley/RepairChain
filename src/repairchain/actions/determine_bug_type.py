c8e5047854b728b6367e63aba49d5e6d428eeceb
ChrisTimperley <christimperley@googlemail.com>
2024-07-01 22:33:56 -0400

    determine_bug_type: added detection of OS Command Injection

--- src/repairchain/actions/determine_bug_type.py
+++ src/repairchain/actions/determine_bug_type.py
@@ -9,10 +9,15 @@ from repairchain.models.bug_type import BugType
 if t.TYPE_CHECKING:
     from repairchain.models.sanitizer_report import SanitizerReport
 
+OS_COMMAND_INJECTION_TITLE = "== Java Exception: com.code_intelligence.jazzer.api.FuzzerSecurityIssueCritical: OS Command Injection"  # noqa: E501
+
 
 def determine_bug_type(report: SanitizerReport) -> BugType:
     report_text = report.contents
     if " global-buffer-overflow " in report_text:
         return BugType.OUT_OF_BOUNDS_WRITE
+
+    if OS_COMMAND_INJECTION_TITLE in report_text:
+        return BugType.OS_COMMAND_INJECTION
 
     raise NotImplementedError
