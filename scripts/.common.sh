1e547545c7f94ce3d31aafa9190a3b9b30f6ebe7
ChrisTimperley <christimperley@googlemail.com>
2024-07-02 22:00:09 -0400

    infra: added .common.sh

---
+++ scripts/.common.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -eu
+
+HERE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+PROJECT_DIR="${HERE_DIR}/.."
+OPENAI_KEY_FILE="${PROJECT_DIR}/.openapi.key"
+LITELLM_CONFIG_FILE="${PROJECT_DIR}/litellm.local.yml"
+
+if ! poetry run which litellm &> /dev/null; then
+  echo "LiteLLM is not installed. Please install LiteLLM before running this script."
+  exit 1
+fi
+
+export OPENAI_API_KEY="$(cat "${OPENAI_KEY_FILE}")"
+export ANTHROPIC_API_KEY=" "
+export AZURE_API_KEY=" "
+export AZURE_API_BASE=" "
+
+cleanup() {
+  kill -9 ${LITELLM_PID}
+}
+
+run_litellm() {
+    poetry run litellm -c $LITELLM_CONFIG_FILE &
+    LITELLM_PID=$!
+    trap cleanup SIGINT SIGTERM EXIT
+}
--- scripts/repair.sh
+++ scripts/repair.sh
@@ -2,35 +2,12 @@
 set -eu
 
 HERE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-PROJECT_DIR="${HERE_DIR}/.."
-OPENAI_KEY_FILE="${PROJECT_DIR}/.openapi.key"
-LITELLM_CONFIG_FILE="${PROJECT_DIR}/litellm.local.yml"
-
-if ! poetry run which litellm &> /dev/null; then
-  echo "LiteLLM is not installed. Please install LiteLLM before running this script."
-  exit 1
-fi
-
-if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 <example_dir>"
-  exit 1
-fi
+source "${HERE_DIR}/.common.sh"
 
 EXAMPLE_DIR="$1"
 pushd "${EXAMPLE_DIR}" &> /dev/null
 
-export OPENAI_API_KEY="$(cat "${OPENAI_KEY_FILE}")"
-export ANTHROPIC_API_KEY=" "
-export AZURE_API_KEY=" "
-export AZURE_API_BASE=" "
-
-cleanup() {
-  kill -9 ${LITELLM_PID}
-}
-
-poetry run litellm -c $LITELLM_CONFIG_FILE &
-LITELLM_PID=$!
-trap cleanup SIGINT SIGTERM EXIT
+run_litellm
 
 poetry run repairchain \
     --log-level INFO \
