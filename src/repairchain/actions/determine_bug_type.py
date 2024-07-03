49b2dd0a21fc23a94e619b3c336416d043e32374
ChrisTimperley <christimperley@googlemail.com>
2024-07-02 20:05:44 -0400

    determine_bug_type: added detection of KASAN slab-out-of-bounds

--- src/repairchain/actions/determine_bug_type.py
+++ src/repairchain/actions/determine_bug_type.py
@@ -11,16 +11,20 @@ if t.TYPE_CHECKING:
 
 ASAN_GLOBAL_BUFFER_OVERFLOW = "AddressSanitizer: global-buffer-overflow"
 ASAN_HEAP_BUFFER_OVERFLOW = "AddressSanitizer: heap-buffer-overflow"
+KASAN_SLAB_OUT_OF_BOUNDS = "KASAN: slab-out-of-bounds"
 OS_COMMAND_INJECTION_TITLE = "== Java Exception: com.code_intelligence.jazzer.api.FuzzerSecurityIssueCritical: OS Command Injection"  # noqa: E501
 
 
 def determine_bug_type(report: SanitizerReport) -> BugType:
     report_text = report.contents
+
     if ASAN_GLOBAL_BUFFER_OVERFLOW in report_text:
         return BugType.OUT_OF_BOUNDS_WRITE
     if ASAN_HEAP_BUFFER_OVERFLOW in report_text:
         return BugType.OUT_OF_BOUNDS_WRITE
+    if KASAN_SLAB_OUT_OF_BOUNDS in report_text and "Write of size" in report_text:
+        return BugType.OUT_OF_BOUNDS_WRITE
     if OS_COMMAND_INJECTION_TITLE in report_text:
         return BugType.OS_COMMAND_INJECTION
 
-    raise NotImplementedError
+    return BugType.UNKNOWN
