25cdd2c3310f403e106f62d98e9ae553961cb97e
Claire Le Goues <clegoues@cs.cmu.edu>
2024-07-05 20:35:13 -0400

    replace minimizer class in diagnose with calls to util minimization

--- src/repairchain/actions/diagnose.py
+++ src/repairchain/actions/diagnose.py
@@ -1,23 +1,29 @@
 from __future__ import annotations
 
+from repairchain.actions.validate import SimplePatchValidator
+from repairchain.models.patch_outcome import PatchOutcome
+from repairchain.util import dd_minimize
+
 __all__ = ("diagnose",)
 
 import typing as t
 
 from dockerblade.stopwatch import Stopwatch
 from loguru import logger
+from sourcelocation import FileHunk
 
 from repairchain.actions.commit_to_diff import commit_to_diff
 from repairchain.actions.determine_bug_type import determine_bug_type
 from repairchain.actions.index_functions import index_functions
 from repairchain.actions.localize_diff import diff_to_functions
 from repairchain.actions.map_functions import map_functions
-from repairchain.actions.minimize_diff import MinimizeForFailure
 from repairchain.models.diagnosis import Diagnosis
+from repairchain.models.diff import Diff
+
+T = t.TypeVar("T")
 
 if t.TYPE_CHECKING:
     from repairchain.models.project import Project
-
 
 def diagnose(project: Project) -> Diagnosis:
     bug_type = determine_bug_type(project.sanitizer_report)
@@ -30,12 +36,18 @@ def diagnose(project: Project) -> Diagnosis:
         stopwatch = Stopwatch()
         logger.info(f"minimizing implicated diff:\n{implicated_diff}")
         stopwatch.start()
-        minimizer = MinimizeForFailure.build(
-            project=project,
-            triggering_diff=implicated_diff,
-            commit=triggering_commit.parents[0],
-        )
-        implicated_diff = minimizer.minimize()
+        # potential FIXME: should this be testing from the commit right before the triggering commit?
+
+        validator = SimplePatchValidator(project, triggering_commit.parents[0])
+
+        def tester(fds: t.Sequence[FileHunk]) -> bool:
+            as_diff = Diff.from_file_hunks(list(fds))
+            outcome = validator.validate(as_diff)
+            return outcome == PatchOutcome.FAILED
+
+        to_minimize: list[FileHunk] = list(implicated_diff.file_hunks)
+        minimized_hunks = dd_minimize(to_minimize, tester)
+        implicated_diff = Diff.from_file_hunks(minimized_hunks)
         time_taken = stopwatch.duration
         logger.info(
             f"minimized implicated diff (took {time_taken:.2f} s):\n{implicated_diff}",
