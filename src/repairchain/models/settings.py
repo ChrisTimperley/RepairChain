3f438c9d80edce8046fe342c2bb8ec11a779d473
ChrisTimperley <christimperley@googlemail.com>
2024-07-05 21:16:48 -0400

    added sanity_check to settings

--- src/repairchain/models/settings.py
+++ src/repairchain/models/settings.py
@@ -17,7 +17,10 @@ class Settings:
         Whether to stop early if a repair is found.
     minimize_failure: bool
         Whether to minimize the failure-inducing diff.
+    sanity_check: bool
+        Whether to perform a sanity check on the failure-inducing diff.
     """
     workers: int = field(default=1)
     stop_early: bool = field(default=True)
     minimize_failure: bool = field(default=True)
+    sanity_check: bool = field(default=True)
