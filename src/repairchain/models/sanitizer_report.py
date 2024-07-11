ec71d48a8c2f18f85376cad9e06d1842d0554c9b
Claire Le Goues <clegoues@cs.cmu.edu>
2024-07-10 21:37:40 -0400

    parse memsan and ubsan

--- src/repairchain/models/sanitizer_report.py
+++ src/repairchain/models/sanitizer_report.py
@@ -90,6 +90,26 @@ class StackTrace(t.Sequence[StackFrame]):
         return all(frame.is_valid() for frame in self.frames)
 
 
+def extract_location(line: str) -> tuple[str, int | None, int | None]:
+    filename = line
+    lineno: int | None = None
+    offset: int | None = None
+    line_index = line.find(":")
+
+    if line_index != -1:
+        filename = line[:line_index]
+        linenostr = line[line_index + 1:]
+        filename, lineno, offset = extract_location(linenostr)
+        offset_index = linenostr.find(":")
+        if offset_index != -1:
+            offset = int(linenostr[offset_index + 1:])
+            lineno = int(linenostr[:offset_index])
+        else:
+            with contextlib.suppress(ValueError):
+                lineno = int(linenostr)
+    return filename, lineno, offset
+
+
 def extract_stack_frame_from_line(line: str) -> StackFrame:
     # TODO prefer partition
     split_index = line.find(" in ")  # FIXME: error handle
@@ -98,21 +118,7 @@ def extract_stack_frame_from_line(line: str) -> StackFrame:
 
     funcname = rhs[:sig_index]
     filename_part = rhs[sig_index + 1:]
-
-    line_index = filename_part.find(":")
-    filename = filename_part
-    lineno: int | None = None
-    offset: int | None = None
-    if line_index != -1:
-        filename = filename_part[:line_index]
-        linenostr = filename_part[line_index + 1:]
-        offset_index = linenostr.find(":")
-        if offset_index != -1:
-            offset = int(linenostr[offset_index + 1:])
-            lineno = int(linenostr[:offset_index])
-        else:
-            with contextlib.suppress(ValueError):
-                lineno = int(linenostr)
+    filename, lineno, offset = extract_location(filename_part)
 
     return StackFrame(
         funcname=funcname,
@@ -122,34 +128,25 @@ def extract_stack_frame_from_line(line: str) -> StackFrame:
     )
 
 
-def parse_kasan(kasan_output: str) -> None:
-    raise NotImplementedError
-
-
-def parse_kfence(kfence_output: str) -> None:
-    raise NotImplementedError
-
-
-def parse_asan(asan_output: str) -> tuple[str, StackTrace]:
-    # Regular expressions to match different parts of the ASan output
-    error_regex = re.compile(r".*ERROR: AddressSanitizer: (.+)")
+def parse_report(report_text: str,
+                 error_regex: re.Pattern[str],
+                 end_regex: re.Pattern[str]) -> tuple[str, StackTrace]:
     stack_frame_regex = re.compile(
         r"\s*#(?P<frame>\d+) "
         r"0x(?P<address>[0-9a-f]+) in ",
     )
-    memory_regex = re.compile(r".*is located (?P<bytes_after>\d+) bytes after (?P<size>\d+)-byte region.*")
 
     error_name = ""
     stack_frames: list[StackFrame] = []
     processing_stack_trace = False
 
-    for line in asan_output.splitlines():
+    for line in report_text.splitlines():
         error_match = error_regex.match(line)
         stack_trace_match = stack_frame_regex.match(line)
-        memory_match = memory_regex.match(line)
+        end_match = end_regex.match(line)
         if error_match:
             error_name = error_match.group(1)
-        elif memory_match or "__libc_start_main" in line:
+        elif end_match or "__libc_start_main" in line:
             break
         elif stack_trace_match:
             processing_stack_trace = True
@@ -159,15 +156,55 @@ def parse_asan(asan_output: str) -> tuple[str, StackTrace]:
     return error_name, stack_trace
 
 
-def parse_memsan(memsan_output: str) -> None:
+def parse_kasan(kasan_output: str) -> tuple[str, StackTrace]:
     raise NotImplementedError
 
 
-def parse_ubsan(ubsan_output: str) -> None:
+def parse_kfence(kfence_output: str) -> tuple[str, StackTrace]:
     raise NotImplementedError
 
 
-def parse_jazzer(jazzer_output: str) -> None:
+def parse_asan(asan_output: str) -> tuple[str, StackTrace]:
+    return parse_report(asan_output,
+                        re.compile(r".*ERROR: AddressSanitizer: (.+)"),
+                        re.compile(r".*is located (?P<bytes_after>\d+) bytes after (?P<size>\d+)-byte region.*"),
+                        )
+
+
+def parse_memsan(memsan_output: str) -> tuple[str, StackTrace]:
+    return parse_report(memsan_output,
+                        re.compile(r".*WARNING: MemorySanitizer: (.+)"),
+                        re.compile(r".*SUMMARY: MemorySanitizer:.*"),
+                        )
+
+
+def parse_ubsan(ubsan_output: str) -> tuple[str, StackTrace]:
+    # runtime error: signed integer overflow: 2147483647 + 1 cannot be represented in type 'int'
+    # SUMMARY: UndefinedBehaviorSanitizer: undefined-behavior file.cpp:7:21
+
+    filename = ""
+    lineno = None
+    offset = None
+    extra_information = ""
+
+    for line in ubsan_output:
+        stripped = line.strip()
+        if "SUMMARY: UndefinedBehaviorSanitizer: undefined-behavior" in stripped:
+            _, _, location = stripped.partition("undefined-behavior ")
+            filename, lineno, offset = extract_location(location)
+        if "runtime error:" in stripped:
+            _, _, extra_information = stripped.partition("error:")
+    frame = StackFrame(
+        funcname="",  # FIXME: bad idea, and need to get the function eventually
+        filename=filename,
+        lineno=lineno,
+        offset=offset,
+    )
+
+    return (extra_information, StackTrace([frame]))
+
+
+def parse_jazzer(jazzer_output: str) -> tuple[str, StackTrace]:
     raise NotImplementedError
 
 
@@ -175,7 +212,7 @@ def parse_jazzer(jazzer_output: str) -> None:
 class SanitizerReport:
     contents: str = field(repr=False)
     sanitizer: Sanitizer
-    error_title: str | None = field(default=None)
+    error_info: str | None = field(default=None)
     bug_type: BugType = BugType.UNKNOWN
     # FIXME move this into a separate subclass
     stack_trace: StackTrace | None = field(default=None)
@@ -210,8 +247,20 @@ class SanitizerReport:
         )
         # TODO need to deal with stack traces that may contain a mix of relative
         # and absolute paths
-        if sanitizer == Sanitizer.ASAN:
-            report.error_title, report.stack_trace = parse_asan(text)
+        match sanitizer:
+            case Sanitizer.KASAN:
+                report.error_info, report.stack_trace = parse_kasan(text)
+            case Sanitizer.KFENCE:
+                report.error_info, report.stack_trace = parse_kfence(text)
+            case Sanitizer.ASAN:
+                report.error_info, report.stack_trace = parse_asan(text)
+            case Sanitizer.MEMSAN:
+                report.error_info, report.stack_trace = parse_memsan(text)
+            case Sanitizer.UBSAN:
+                report.error_info, report.stack_trace = parse_ubsan(text)
+            case Sanitizer.JAZZER:
+                report.error_info, report.stack_trace = parse_jazzer(text)
+
         return report
 
     @classmethod
