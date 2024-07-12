a7cb2fd54d991315cf99c913d621980ac9f50683
Claire Le Goues <clegoues@cs.cmu.edu>
2024-07-12 16:12:34 -0400

    increase size missing call to LLM, but should work if we have it (famous last words)

--- src/repairchain/strategies/generation/template/increase_size.py
+++ src/repairchain/strategies/generation/template/increase_size.py
@@ -4,31 +4,14 @@ from dataclasses import dataclass
 import kaskara
 from overrides import overrides
 from sourcelocation.diff import Diff
+from sourcelocation.fileline import FileLine
+from sourcelocation.location import FileLocation, Location
 
 from repairchain.actions.commit_to_diff import get_file_contents_at_commit
-from repairchain.models.bug_type import Sanitizer
 from repairchain.models.diagnosis import Diagnosis
-from repairchain.models.sanitizer_report import SanitizerReport
-from repairchain.models.stack_trace import StackFrame
+from repairchain.models.replacement import Replacement
 from repairchain.strategies.generation.template.base import TemplateGenerationStrategy
 
-
-def get_declarations(report: SanitizerReport) -> list[kaskara.statements.Statement]:
-    match report.sanitizer:
-        case Sanitizer.KASAN:
-            raise NotImplementedError
-        case Sanitizer.KFENCE:
-            raise NotImplementedError
-        case Sanitizer.ASAN:
-            raise NotImplementedError
-        case Sanitizer.MEMSAN:
-            raise NotImplementedError
-        case Sanitizer.UBSAN:
-            raise NotImplementedError
-        case Sanitizer.JAZZER:
-            raise NotImplementedError
-        case Sanitizer.UNKNOWN:
-            raise NotImplementedError
 
 @dataclass
 class IncreaseSizeStrategy(TemplateGenerationStrategy):
@@ -39,67 +22,27 @@ class IncreaseSizeStrategy(TemplateGenerationStrategy):
 
     KASAN has an allocated-stack
     KFENCE should have an allocated-stack
-    ASAN does too -- 
+    ASAN does too --
     UBSAN does array OOB --- and it doesnt have the allocated stack but it does have the type
     """
 
     diagnosis: Diagnosis
-    declarations_to_repair: list[kaskara.statements.Statement]
-    accesses_to_repair: list[kaskara.statements.Statement]
+    declarations_to_repair: list[tuple[kaskara.statements.Statement, kaskara.functions.Function, FileLine, str]]
+    accesses_to_repair: list[tuple[kaskara.statements.Statement, kaskara.functions.Function, FileLine, str]]
 
     @classmethod
-    def _get_overflow_location(cls, report: SanitizerReport, diagnosis: Diagnosis) -> StackFrame:
-        location = report.error_location
-        assert location is not None
-        if not location.has_lineno():
-            # FIXME: I think this can only happen when things aren't symbolized
-            # punting for now
-            return location
-        if location.has_funcname() and not location.has_filename():  # common
-            # a complete location has a filename, funcname, and lineno
-            # not uncommon to have a function name but no filename
-            # if we have the function name but no line number, can cross check with the
-            # commit history, maybe???
-            implicated_functions = diagnosis.implicated_functions_at_head
-            assert implicated_functions is not None
-            for function in implicated_functions:
-                if function.name == location.funcname:
-                    location.filename = function.filename
-                    return location
-            # if that didn't work, try the nuclear option
-            index = diagnosis.index_at_head
-            assert index is not None
-            for function in index.functions:
-                if function.name == location.funcname:
-                    location.filename = function.filename
-                    return location
-        return location
-
-    @classmethod
-    def _get_potential_declarations(cls, location: StackFrame, report: SanitizerReport, diagnosis: Diagnosis) -> list[kaskara.statements.Statement]:
-        # location is the location of the overflow, should reference at least one variable
-        # look for it in the allocation stack
+    def _get_variables(cls,
+                        stmt: kaskara.statements.Statement,
+                        diagnosis: Diagnosis) -> frozenset[str]:
+        vars_read: frozenset[str] = frozenset([])
         head_index = diagnosis.index_at_head
         assert head_index is not None
-
-        vars_read: frozenset[str] = frozenset([])
-        for stmt in head_index.statements.at_line(location.file_line):
-            vars_read.union(frozenset(stmt.reads if hasattr(stmt, "reads") else []))
         # TODO: filter variables by type to cut it down
-
-        allocated_stack = report.alloc_stack_trace
-        declaring_stmts: list[kaskara.statements.Statement] = []
-
-        for frame in allocated_stack.frames:
-            if frame.is_complete():
-                head_stmts = head_index.statements._statements
-                stmts = head_index.statements.at_line(frame.file_line)
-                for stmt in stmts:
-                    if isinstance(stmt, kaskara.clang.analysis.ClangStatement) and len(vars_read.intersection(stmt.declares)) > 0:
-                        declaring_stmts.append(stmt)
-        if len(declaring_stmts) == 0:
-            raise NotImplementedError  # try something else.  UBSan especially is going to be a problem here
-        return declaring_stmts
+        if isinstance(stmt, kaskara.clang.analysis.ClangStatement):
+            vars_read.union(frozenset(stmt.reads if hasattr(stmt, "reads") else []))
+        if isinstance(stmt, kaskara.clang.analysis.ClangStatement) and len(vars_read) == 0:
+            vars_read.union(frozenset(stmt.writes if hasattr(stmt, "writes") else []))
+        return vars_read
 
     @classmethod
     def build(cls, diagnosis: Diagnosis) -> t.Self:
@@ -108,24 +51,49 @@ class IncreaseSizeStrategy(TemplateGenerationStrategy):
         # So I need to know the thing that was accessed/where, and where it was declared.
         # then either modify the declaration, or
         # get the location of the out of bounds read/array access
-        report = diagnosis.sanitizer_report
-        location = cls._get_overflow_location(report, diagnosis)
-        if not location.is_complete():  # FIXME: is it possible for us not to get a location?
-            raise NotImplementedError # try to do something to grok the location
-            # this will often involve reconstructing the file, since many of the
-            # sanitizers report the function name but not the filename
-            # so we need to find the file to have a file-line location
-        stmts = cls._get_potential_declarations(location, report, diagnosis)
         head_index = diagnosis.index_at_head
+        assert head_index is not None
+        report = diagnosis.sanitizer_report
+        location = cls._get_error_location(report, diagnosis)
+        vars_of_interest: frozenset[str] = frozenset([])
+        for statement in head_index.statements.at_line(location.file_line):
+            vars_of_interest = vars_of_interest.union(cls._get_variables(statement, diagnosis))
+        stmts = cls._get_potential_declarations(vars_of_interest, report, diagnosis)
+
+        assert location is not None
+        assert location.lineno is not None
+        assert location.filename is not None
+
+        baseloc = Location(location.lineno, location.offset if location.offset is not None else 0)
+        as_loc = FileLocation(location.filename, baseloc)
+        fn = head_index.functions.encloses(as_loc)
+        assert fn is not None
+        file_contents = get_file_contents_at_commit(
+                    diagnosis.project.repository.active_branch.commit,
+                    fn.filename,
+                )
+        access = head_index.statements.at_line(location.file_line)
+        accesses = [(stmt, fn, location.file_line, file_contents) for stmt in access]
         assert location is not None
         assert head_index is not None
         return cls(
             diagnosis=diagnosis,
             declarations_to_repair=stmts,
-            accesses_to_repair=list(head_index.statements.at_line(location.file_line)),
+            accesses_to_repair=accesses,
         )
+
+    def _generate_for_statement(
+        self,
+        stmt: kaskara.statements.Statement,
+        diagnosis: Diagnosis,
+    ) -> Diff:
+
+        # this statement should be a declaration statement, so all we need to do
+        # is get a new one and replace it
+        source = "PLACEHOLDER if( PLACEHOLDER > 500) { return; }\n"  # FIXME: call out to LLM
+        repl = Replacement(stmt.location, source)
+        return diagnosis.project.sources.replacements_to_diff([repl])
 
     @overrides
     def run(self) -> list[Diff]:
-
-        raise NotImplementedError
+        return [self._generate_for_statement(stmt[0], self.diagnosis) for stmt in self.declarations_to_repair]
