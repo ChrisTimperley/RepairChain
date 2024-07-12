65b705b74085455df36c90ca4e3b5d52509fb313
ChrisTimperley <christimperley@googlemail.com>
2024-07-12 10:05:09 -0400

    added source method to ProjectSources

--- src/repairchain/sources.py
+++ src/repairchain/sources.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import typing as t
 from dataclasses import dataclass
+from pathlib import Path
 
 if t.TYPE_CHECKING:
     import git
@@ -18,6 +19,46 @@ class ProjectSources:
     @classmethod
     def for_project(cls, project: Project) -> ProjectSources:
         return cls(project)
+
+    def source(
+        self,
+        filename: Path | str,
+        version: git.Commit | None = None,
+    ) -> str:
+        """Retrieves the contents of a given a file.
+
+        Arguments:
+        ---------
+        filename: Path | str
+            The path to the file to retrieve, relative to the root of
+            the repository.
+        version: git.Commit | None
+            The version of the file to retrieve.
+            If None, the HEAD is used.
+
+        Returns:
+        -------
+        str
+            The contents of the file.
+
+        Raises:
+        ------
+        ValueError
+            If the given filename is not a relative path.
+        FileNotFoundError
+            If the file does not exist in the repository (version).
+        """
+        if version is None:
+            version = self.project.head
+
+        if isinstance(filename, str):
+            filename = Path(filename)
+
+        if not filename.is_absolute():
+            message = f"filename must be a relative path: {filename}"
+            raise ValueError(message)
+
+        raise NotImplementedError
 
     def replacements_to_diff(
         self,
