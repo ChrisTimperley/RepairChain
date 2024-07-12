033c44a213f4c24e1935c104ab14e78975a04ea4
ChrisTimperley <christimperley@googlemail.com>
2024-07-12 12:10:04 -0400

    test: updated fixture to skip test if in CI (without access to secret versatil repos)

--- test/conftest.py
+++ test/conftest.py
@@ -8,9 +8,14 @@ TEST_DIR = Path(__file__).parent
 PROJECT_DIR = TEST_DIR.parent
 EXAMPLES_DIR = PROJECT_DIR / "examples"
 MOCK_CP_DIR = EXAMPLES_DIR / "mock-cp"
+MOCK_CP_REPO_DIR = MOCK_CP_DIR / "mock-cp-src/src/samples"
 
 
 @pytest.fixture
 def local_repo_mockcp() -> t.Iterator[git.Repo]:
-    repo_dir = MOCK_CP_DIR / "mock-cp-src/src/samples"
+    repo_dir = MOCK_CP_REPO_DIR
+
+    if not repo_dir.exists():
+        pytest.skip(f"skipping test: required directory {repo_dir} does not exist")
+
     yield git.Repo(repo_dir)
--- test/test_sources.py
+++ test/test_sources.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+import pytest
+
 from repairchain.sources import SourceFileVersion
 
 
