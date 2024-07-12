76da66c60fe70c330934ebf2030fc89e429091b7
ChrisTimperley <christimperley@googlemail.com>
2024-07-12 18:54:29 -0400

    pytest: added example_project_factory for integration tests

--- test/conftest.py
+++ test/conftest.py
@@ -1,8 +1,14 @@
+import contextlib
+import os
 import typing as t
 from pathlib import Path
 
+import docker
 import git
 import pytest
+
+from repairchain.models.project import Project
+from repairchain.models.settings import Settings
 
 TEST_DIR = Path(__file__).parent
 PROJECT_DIR = TEST_DIR.parent
@@ -19,3 +25,34 @@ def local_repo_mockcp() -> t.Iterator[git.Repo]:
         pytest.skip(f"skipping test: required directory {repo_dir} does not exist")
 
     yield git.Repo(repo_dir)
+
+
+@pytest.fixture
+def example_project_factory() -> t.Callable[[str, Settings | None], t.Iterator[Project]]:
+    """Provides a factory for creating Project instances for example projects."""
+
+    @contextlib.contextmanager
+    def factory(example: str, settings: Settings | None = None) -> t.Iterator[Project]:
+        if "DOCKER_HOST" not in os.environ:
+            pytest.skip("skipping test: DOCKER_HOST environment variable is not set")
+
+        example_dir = EXAMPLES_DIR / example
+        if not example_dir.exists():
+            pytest.skip(f"skipping test: required directory {example_dir} does not exist")
+
+        project_file = example_dir / "project.json"
+        if not project_file.exists():
+            pytest.skip(f"skipping test: required file {project_file} does not exist")
+
+        if settings is None:
+            settings = Settings.from_env()
+
+        with Project.load(project_file, settings=settings) as project:
+            try:
+                project.docker_daemon.client.images.get(project.image)
+            except docker.errors.ImageNotFound:
+                pytest.skip(f"skipping test: required image {project.image} does not exist")
+
+            yield project
+
+    return factory
