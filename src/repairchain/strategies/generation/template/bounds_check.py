de3e7eb357b463b0ab9937bd48686d842af357c8
Claire Le Goues <clegoues@cs.cmu.edu>
2024-07-14 10:31:01 -0400

    Cleanup/organize, restrict indexing to project files in bounds check

--- src/repairchain/strategies/generation/template/bounds_check.py
+++ src/repairchain/strategies/generation/template/bounds_check.py
@@ -24,6 +24,7 @@ if t.TYPE_CHECKING:
     from repairchain.models.diagnosis import Diagnosis
     from repairchain.models.diff import Diff
     from repairchain.models.sanitizer_report import StackTrace
+    from repairchain.sources import ProjectSources
 
 
 @dataclass
@@ -62,38 +63,42 @@ class BoundsCheckStrategy(TemplateGenerationStrategy):
         self,
         stmt: kaskara.statements.Statement,
     ) -> list[Diff]:
-        diffs: list[Diff] = []
-        helper = CodeHelper(self.llm)
-
         if self.index is None:
             logger.warning("Unexpected incomplete diagnosis in bounds check template.")
             return []
+        diffs: list[Diff] = []
+        helper = CodeHelper(self.llm)
 
         stmt_loc = FileLocation(stmt.location.filename, stmt.location.start)
         fn = self.index.functions.encloses(stmt_loc)
-        if fn is not None:
-            fn_src = self._fn_to_text(fn)
+        if fn is None:
+            return []
 
-            reads = frozenset(stmt.reads if hasattr(stmt, "reads") else [])
-            for varname in reads:  # would be super cool to know the type, but who has the time, honestly.
-                output = helper.help_with_bounds_check(fn_src, stmt.content, varname)
-                if output is not None:
-                    for line in output.code:
-                        repl = Replacement(stmt.location, line.line)
-                        diffs.append(self.diagnosis.project.sources.replacements_to_diff([repl]))
+        fn_src = self._fn_to_text(fn)
+
+        reads = frozenset(stmt.reads if hasattr(stmt, "reads") else [])
+        for varname in reads:  # TODO: consider type info when available
+            output = helper.help_with_bounds_check(fn_src, stmt.content, varname)
+            if output is None:
+                continue
+            for line in output.code:
+                repl = Replacement(stmt.location, line.line)
+                diffs.append(self.diagnosis.project.sources.replacements_to_diff([repl]))
         return diffs
 
-    def _generate_for_function(self, function: kaskara.functions.Function, stack_trace: StackTrace) -> list[Diff]:
+    def _generate_for_function(self,
+                               function: kaskara.functions.Function,
+                               stack_trace: StackTrace) -> list[Diff]:
         diffs: list[Diff] = []
-        logger.debug(f"generating bounds check repairs in function: {function}")
         if self.index is None:
             logger.warning("Unexpected incomplete diagnosis in bounds check template.")
             return []
 
         for frame in stack_trace.restrict_to_function(function):
-            if frame is not None and frame.file_line is not None:
-                for statement in self.index.statements.at_line(frame.file_line):
-                    diffs += self._generate_for_statement(statement)
+            if frame is None or frame.file_line is None:
+                continue
+            for statement in self.index.statements.at_line(frame.file_line):
+                diffs += self._generate_for_statement(statement)
 
         return diffs
 
@@ -101,23 +106,31 @@ class BoundsCheckStrategy(TemplateGenerationStrategy):
     def run(self) -> list[Diff]:
         diffs: list[Diff] = []
         implicated_functions = self.diagnosis.implicated_functions_at_head
-        if implicated_functions is not None and self.report.call_stack_trace:
-            logger.debug(f"implicated_functions: {len(implicated_functions)}")
 
-            # filter the stack trace to only those functions that are implicated
-            stack_trace = self.report.call_stack_trace
-            stack_trace = stack_trace.restrict_to_functions(implicated_functions)
-            logger.debug(f"filtered stack trace: {stack_trace}")
-            indexer: KaskaraIndexer = self.diagnosis.project.indexer
-            # find the set of localized functions
-            functions_in_trace = stack_trace.functions()
-            functions_to_repair = [
-               f for f in implicated_functions if f.name in functions_in_trace
-            ]
-            logger.debug(f"localized_functions: {len(functions_to_repair)}")
-            self.index = indexer.run(version=self.diagnosis.project.head,
-                                     restrict_to_files=list(stack_trace.filenames()))
+        if implicated_functions is None or len(self.report.call_stack_trace) == 0:
+            logger.warning("incomplete diagnosis info in bounds check strategy, skipping")
+            return []
 
-            for function in functions_to_repair:
-                diffs += self._generate_for_function(function, stack_trace)
+        logger.debug(f"implicated_functions: {len(implicated_functions)}")
+
+        # filter the stack trace to only those functions that are implicated
+        stack_trace = self.report.call_stack_trace
+        stack_trace = stack_trace.restrict_to_functions(implicated_functions)
+
+        # find the set of localized functions
+        functions_to_repair = [
+            f for f in implicated_functions if f.name in stack_trace.functions()
+        ]
+
+        # index as necessary
+        sources: ProjectSources = self.diagnosis.project.sources
+        indexer: KaskaraIndexer = self.diagnosis.project.indexer
+        filenames = [
+            f for f in stack_trace.filenames() if sources.exists(f, self.diagnosis.project.head)
+        ]
+        self.index = indexer.run(version=self.diagnosis.project.head,
+                                restrict_to_files=filenames)
+
+        for function in functions_to_repair:
+            diffs += self._generate_for_function(function, stack_trace)
         return diffs
