5956b631da82dac9a7ea8ffc0729d36944df5cb1
Claire Le Goues <clegoues@cs.cmu.edu>
2024-07-14 10:18:39 -0400

    reorganize init mem slightly, don't index files that don't exist

--- src/repairchain/strategies/generation/template/init_mem.py
+++ src/repairchain/strategies/generation/template/init_mem.py
@@ -46,29 +46,46 @@ class InitializeMemoryStrategy(TemplateGenerationStrategy):
         location = cls._get_error_location(diagnosis)
         return location is not None
 
-    @overrides
-    def run(self) -> list[Diff]:
-        repls: list[Diff] = []
+    def _set_index(self) -> None:
         location = self._get_error_location(self.diagnosis)
-        helper = CodeHelper(self.llm)
         indexer: KaskaraIndexer = self.diagnosis.project.indexer
 
         if location is None or location.filename is None or location.file_line is None:
-            return []
+            return
+        if not self.diagnosis.project.sources.exists(location.filename,
+                                                     self.diagnosis.project.head,
+                                                     ):
+            return
         self.index = indexer.run(version=self.diagnosis.project.head,
                                      restrict_to_files=list(location.filename))
+        return
+
+    @overrides
+    def run(self) -> list[Diff]:
+        self._set_index()
+        location = self._get_error_location(self.diagnosis)
+        if location is None or location.file_line is None or self.index is None:
+            return []
+
+        repls: list[Diff] = []
+        helper = CodeHelper(self.llm)
+
         stmts_at_error_location = self.index.statements.at_line(location.file_line)
 
         for stmt in stmts_at_error_location:
             stmt_loc = FileLocation(stmt.location.filename, stmt.location.start)
             fn = self.index.functions.encloses(stmt_loc)
-            if fn is not None:
-                fn_src = self._fn_to_text(fn)
-                output = helper.help_with_memory_initialization(fn_src, stmt.content)
-                if output is not None:
-                    for line in output.code:
-                        combine = line.line + "\n" + stmt.content
-                        repl = Replacement(stmt.location, combine)
-                        repls.append(self.diagnosis.project.sources.replacements_to_diff([repl]))
+            if fn is None:
+                continue
+            fn_src = self._fn_to_text(fn)
+            output = helper.help_with_memory_initialization(fn_src, stmt.content)
+
+            if output is None:
+                continue
+
+            for line in output.code:
+                combine = line.line + "\n" + stmt.content
+                repl = Replacement(stmt.location, combine)
+                repls.append(self.diagnosis.project.sources.replacements_to_diff([repl]))
 
         return repls
