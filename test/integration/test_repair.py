f16b09cdc37b97b262738fe59b081c77f0219c49
ChrisTimperley <christimperley@googlemail.com>
2024-07-13 23:46:19 -0400

    test: added placeholder for integration testing of template-based repair

--- test/integration/test_repair.py
+++ test/integration/test_repair.py
@@ -22,3 +22,25 @@ def test_reversion_repair(
     with example_project_factory(example) as project:
         found_patches = list(repair(project))
         assert found_patches
+
+
+@pytest.mark.skip(reason="template repair isn't in place")
+@pytest.mark.parametrize("example", [
+    "jenkins",
+    "linux",
+    "mock-cp",
+    "nginx",
+])
+def test_template_repair(
+    example_project_factory,
+    test_settings,
+    example,
+) -> None:
+    settings = test_settings
+    settings.workers = 8
+    settings.enable_template_repair = True
+    settings.enable_reversion_repair = False
+    settings.enable_yolo_repair = False
+    with example_project_factory(example) as project:
+        found_patches = list(repair(project))
+        assert found_patches
