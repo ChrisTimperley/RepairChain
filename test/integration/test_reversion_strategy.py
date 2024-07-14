3a1ab1821a5cdbe07df468b0771ba8712eeabab7
ChrisTimperley <christimperley@googlemail.com>
2024-07-13 21:16:50 -0400

    test: added test for reversion repair on all example projects

--- test/integration/test_reversion_strategy.py
+++ test/integration/test_reversion_strategy.py
@@ -3,15 +3,22 @@ import pytest
 from repairchain.actions.repair import repair
 
 
-@pytest.mark.xfail(reason="rebasing fails to produce a valid patch")
-def test_repair_with_reversion_on_mockcp(
+@pytest.mark.parametrize("example", [
+    "jenkins",
+    "linux",
+    pytest.param("mock-cp", marks=pytest.mark.xfail(reason="rebasing fails to produce valid patch on mock-cp")),
+    "nginx",
+])
+def test_reversion_repair(
     example_project_factory,
     test_settings,
+    example,
 ) -> None:
     settings = test_settings
+    settings.workers = 4
     settings.enable_template_repair = False
     settings.enable_reversion_repair = True
     settings.enable_yolo_repair = False
-    with example_project_factory("mock-cp") as project:
+    with example_project_factory(example) as project:
         found_patches = list(repair(project))
         assert found_patches
