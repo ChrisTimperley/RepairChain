b2161a3d0b2b6dbee63bb48b4471d5adf19e6db8
ChrisTimperley <christimperley@googlemail.com>
2024-07-15 23:58:13 -0400

    added super basic test that isn't sufficient

--- test/integration/test_util.py
+++ test/integration/test_util.py
@@ -1,12 +1,27 @@
 import typing as t
 
-from repairchain.util import statements_in_function
+from repairchain.util import (
+    revert_diff,
+    statements_in_function,
+)
 
 
 if t.TYPE_CHECKING:
     import kaskara
 
     from repairchain.indexer import KaskaraIndexer
+
+
+def test_revert_diff(
+    example_project_factory,
+) -> None:
+    with example_project_factory("mock-cp") as project:
+        repo = project.repository
+        original_implicated_diff = project.original_implicated_diff
+        triggering_commit = project.triggering_commit
+        triggering_commit_parent = triggering_commit.parents[0]
+
+        assert revert_diff(revert_diff(original_implicated_diff)) == original_implicated_diff
 
 
 def test_statements_in_function(
